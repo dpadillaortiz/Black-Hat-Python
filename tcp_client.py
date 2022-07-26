ddff6be8e17f0cce682e82f82d896aaa67d08895
Daniel <padillaortiz.daniel099@gmail.com>
2022-07-26 01:29:00 -0400

    made the code in tcp and udp client into a function

--- tcp_client.py
+++ tcp_client.py
@@ -2,18 +2,26 @@ import socket
 
 target_host = "0.0.0.0"
 target_port = 9998
+example_host = 'www.google.com'
+example_port = 80
+example_send = b'GET / HTTP/1.1\r\nHost: google.com\r\n\r\n'
 
-# creade a socket object
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def tcp_client(target, port, data_to_send):
+    # create a socket object
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# connect the client 
-client.connect((target_host, target_port))
+    # connect the client 
+    client.connect((target, port))
 
-# send some data
-client.send(b'Hello!')
+    # send some data
+    client.send(data_to_send)
 
-# recieve some data
-response = client.recv(4096)
+    # recieve some data
+    response = client.recv(4096)
 
-print(response.decode())
-client.close()
+    print(response.decode())
+
+    client.close()
+
+
+tcp_client(example_host, example_port, example_send)
--- udp_client.py
+++ udp_client.py
@@ -2,15 +2,20 @@ import socket
 
 target_host = "127.0.0.1"
 target_port = 9997
+data_to_send = b'AAABBBCCC'
 
-# creade a socket object
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def udp_client(host, port):
+    # creade a socket object
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# send some data
-client.sendto(b'AAABBBCCC',(target_host, target_port))
+    # send some data
+    client.sendto(data_to_send, (host, port))
 
-# recieve some data
-data, addr = client.recvfrom(4096)
+    # recieve some data
+    data, addr = client.recvfrom(4096)
 
-print(data.decode())
-client.close()
+    print(data.decode())
+    print(addr)
+    client.close()
+
+udp_client(target_host, target_port)
